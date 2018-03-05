3668a6cccbc44dfc822de19c1964c6eca494ec73
Irving Cabello <irvollo@gmail.com>
2018-03-05 17:56:32 -0600

    State with timeout working

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
-import { Button, Message} from 'semantic-ui-react'
+import { Button, Message} from 'semantic-ui-react';
+import  Seed   from './components/Seed' 
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showSeedMenu: false,
+      loader: false
+    }
+    this.showSeedMenu = this.showSeedMenu.bind(this);
+    this.showLoader = this.showLoader.bind(this);
+  }
+ 
+  showSeedMenu() {
+    this.setState({
+      showSeedMenu: true,
+      loader: false
+    });
+  }
+
+  showLoader(){
+    this.setState({
+      showSeedMenu: false,
+      loader: true
+    });
+    setTimeout(this.showSeedMenu, 5000)
+  }
+
   render() {
     return (
       <div className="App">
-        <Message>
-          <Message.Header>
-            Online Seed Generator
-          </Message.Header>
-          <p>
-            Welcome to the Free IOTA Online Seed Generator, to generate your new seed please press the button.
-          </p>
-        </Message>
-        <Button>Click Here</Button>
+        <div className="Welcome">
+          <Message>
+            <Message.Header>
+              Online Seed Generator
+            </Message.Header>
+            <p>
+              Welcome to the Free IOTA Online Seed Generator, to generate your new seed please press the button.
+            </p>
+          </Message>
+        </div>
+        <div className="SeedMenu">
+        {this.state.showSeedMenu ? <Seed /> : <Button onClick={this.showLoader} >Click Here</Button>}
+        {this.state.loader ? <p>Generating your new seed</p> : null }
+        </div>
       </div>
     );
-  }
-  handleClick() {
-    this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
-    }));
   }
 }
 
--- src/components/Seed.js
+++ src/components/Seed.js
@@ -1,14 +1,19 @@
+import React, { Component } from 'react';
+import { Input } from 'semantic-ui-react'
+
 class Seed extends Component {
+
     render() {
       return (
         <div className="Seed">
-          
+          <p> This is your new seed:</p>
+          <Input
+            action={{ color: 'teal', labelPosition: 'right', icon: 'copy', content: 'Copy' }}
+            defaultValue='IM9A9HACKER9AND9IM9GONNA9STEAL9ALL9YOUR9IOTAS'
+          />
         </div>
       );
     }
-    handleClick() {
-      this.setState(prevState => ({
-        isToggleOn: !prevState.isToggleOn
-      }));
-    }
   }
+
+export default Seed;

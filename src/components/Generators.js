28f259ab9bd4a5786b0f7ec456954ff7d3763d11
Irving Cabello <irvollo@gmail.com>
2018-03-08 18:30:52 -0600

    Accordion with generation Methods

---
+++ src/components/Generators.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import { Accordion, Icon, Segment, Image } from 'semantic-ui-react'
+
+export default class Generators extends Component {
+  state = { activeIndex: 0 }
+
+  handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = this.state
+    const newIndex = activeIndex === index ? -1 : index
+
+    this.setState({ activeIndex: newIndex })
+  }
+
+  render() {
+    const { activeIndex } = this.state
+
+    return (
+      <Accordion styled>
+        <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Linux - Command Line
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 0}>
+            <p>Run this in your command line and write your seed in a safe place:</p>
+            <Segment>
+                cat /dev/urandom |tr -dc A-Z9|head -c$	&#123;1:-81	&#125;
+            </Segment>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 1} index={1} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Mac OSx - Command Line
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 1}>
+          <p>Run this in your command line and write your seed in a safe place:</p>
+            <Segment>
+                cat /dev/urandom |LC_ALL=C tr -dc 'A-Z9' | fold -w 81 | head -n 1
+            </Segment>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 2} index={2} onClick={this.handleClick}>
+          <Icon name='dropdown' />
+          Windows - KeePass
+        </Accordion.Title>
+        <Accordion.Content active={activeIndex === 2}>
+          <p>
+            At this moment, Windows doesn't have a reliable command line method. But you can download <a href="https://keepass.info/">KeePass</a> and
+            generate your seed with the following options:
+          </p>
+          <Segment>
+            <Image src='../img/keepass.png' size="medium" />
+          </Segment>
+        </Accordion.Content>
+
+        <Accordion.Title active={activeIndex === 3} index={3} onClick={this.handleClick}>
+            <Icon name='dropdown' />
+            Dice
+            </Accordion.Title>
+            <Accordion.Content active={activeIndex === 3}>
+                <p>
+                You can use either one or two dice, although two will go much faster. Simply roll once for the column and then again for the row.
+                If you end up in the sixth row, re-roll for the row. If you end up in the last three columns on the fifth row, try again.
+                </p>
+                <Segment>
+                    <Image src='../img/dice.png' size="medium" />
+                </Segment>
+                <a href="https://www.reddit.com/r/Iota/comments/7hos4t/secure_dice_roll_seed_generator_template/">Credits to ThroughEnd</a>
+            </Accordion.Content>
+        </Accordion>
+    )
+  }
+}
--- src/components/StopModal.js
+++ src/components/StopModal.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import  {  Modal, Header, Button }  from 'semantic-ui-react';
+import  {  Modal, Header, Message }  from 'semantic-ui-react';
+import Generators from './Generators'
 
 class StopModal extends Component {
 
@@ -13,23 +14,22 @@ class StopModal extends Component {
         <Modal 
               trigger={<i className={`${this.icon} icon large`}></i>} 
               style={{ marginTop: '4rem', marginLeft: '10%', width:'75%' }}
+              closeIcon
               >    
               <Modal.Header><Header color="red">STOP! <i className="tiny icon warning sign"></i></Header></Modal.Header>
-              <Modal.Content >
+              <Modal.Content>
                 <Modal.Description>
                   <Header>IOTA Online Seed Generators are unsecure, please don't use this seed under any circumstance.</Header>
-                  <p>Using Online seed generators had result in over <b>$4M</b> USD stolen in the past</p>
                   <p>This are some secure alternatives to generate your seed offline:</p>
-                  <ul>
-                      <a><li>Mac OSx Command Line</li></a>
-                      <a><li>Linux Command Line</li></a>
-                      <a><li>Dice</li></a>
-                  </ul>
+                  <Generators />
+                  <Message >
+                    <p> Hey! If this helped you how about sending a  
+                      <span role="img" aria-label="Coffe"> ☕ </span>  (1 MIOTA) to this Address and support the dev:
+                      <b> GXNMVRWJSFMGKOVCMOGICITNYUDKOJMSZTMTQOCVAKSPBHSDP9PIGCGFSMSFNPBNMKREUOOFQR9FUZUL9QGSVQKDUX</b>
+                    </p>
+                  </Message>
                 </Modal.Description>
               </Modal.Content>
-              <Modal.Actions>
-                <Button positive icon='checkmark' labelPosition='center' content='Understood' />
-              </Modal.Actions>
         </Modal>
       );
     }
